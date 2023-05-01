fbd1f258f96e557ba56eb8acdc2dfca83aaae8dd
AbrahamRH <abrahamrzhz@gmail.com>
2023-04-30 21:26:25 -0500

    Agregando en test individuales

--- Algorithms.py
+++ Algorithms.py
@@ -17,10 +17,25 @@ KEY_512 = os.urandom(64)
 NONCE = os.urandom(16)
 
 
-def testAlgorithm(cipher, vector):
-    #TODO: Obtener vectores de prueba y aplicar el for aqui
-    msg = b"Just a test, continue with random text xD: adkfjasfda0ehfaoif9212839182312u31o2hjkhvk"
+def testChacha(vectores):
+    msg = vectores
+    cipher = Cipher(algorithms.ChaCha20(KEY_256,NONCE), mode=None, backend=default_backend())
+    encryptor = cipher.encryptor()
+    decryptor = cipher.decryptor()
+    encryption_time_start = process_time()
+    cipherText = encryptor.update(msg) + encryptor.finalize()
+    encryption_time_end = process_time()
+    encryption_time = encryption_time_end - encryption_time_start 
 
+    decryption_time_start = process_time()
+    plaintext = decryptor.update(cipherText) + decryptor.finalize()
+    decryption_time_end = process_time()
+    decryption_time = decryption_time_end - decryption_time_start
+    return (encryption_time,decryption_time)
+    
+def testECB(vectores):
+    msg = vectores
+    cipher = Cipher(algorithms.AES(KEY_256), mode=modes.ECB(), backend=default_backend())
     encryptor = cipher.encryptor()
     decryptor = cipher.decryptor()
 
@@ -29,30 +44,29 @@ def testAlgorithm(cipher, vector):
     encryption_time_end = process_time()
     encryption_time = encryption_time_end - encryption_time_start 
 
-
+    decryption_time_start = process_time()
     decryption_time_start = process_time()
     plaintext = decryptor.update(cipherText) + decryptor.finalize()
     decryption_time_end = process_time()
-
     decryption_time = decryption_time_end - decryption_time_start
-
     return (encryption_time,decryption_time)
 
 
 
 
 def getData():
-    chacha20 = Cipher(algorithms.ChaCha20(KEY_256,NONCE), mode=None, backend=default_backend())
-    AES_ECB = Cipher(algorithms.AES(KEY_256), mode=modes.ECB(), backend=default_backend())
-    AES_GCM = Cipher(algorithms.AES(KEY_256), mode=modes.GCM(NONCE), backend=default_backend())
+    #AES_GCM = Cipher(algorithms.AES(KEY_256), mode=modes.GCM(NONCE), backend=default_backend())
+    #TODO: tomar los vectores
+    #v1 = []
+    v1 = b"Vectores de prueba xd, ahora pura basurasjfaoi 231,123 123 123 "
+    testChacha(v1)
 
-    chachaTime = testAlgorithm(chacha20, "hola") # Esto deberia ser una lista con muchos tiempos(?
-    print(chachaTime)
+
+
 
 
 if __name__ == "__main__":
     getData()
-    print("hola")
 
 
 
